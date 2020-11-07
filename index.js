dfdd10faadb51f8480ffa68510194eb840ff1bee
Mārtiņš Zernis <61688268+maox27@users.noreply.github.com>
2020-11-07 22:22:24 +0200

    Fetching movie data

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const respones = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "edb2b3a0",
+            s: "avengers" // stands for search property
+        }
+    });
+    console.log(respones.data);
+};
+fetchData();
