b414e8040f3aeb9ac81994ce22b9b5c05da657cc
Mārtiņš Zernis <61688268+maox27@users.noreply.github.com>
2020-11-08 09:06:10 +0200

    Searching the API on Input Change

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const respones = await axios.get("http://www.omdbapi.com/", {
         params: {
             apikey: "edb2b3a0",
-            i: "tt0848228" // get information about single movie
+            s: searchTerm // search for particular movie, not ID ("i")
         }
     });
     console.log(respones.data);
 };
-fetchData();
+
+const input = document.querySelector("input");
+input.addEventListener("input", (event) => { //input event listener gives a chance to see every time user change a text.
+    fetchData(event.target.value); // gives access to changed text
+});
